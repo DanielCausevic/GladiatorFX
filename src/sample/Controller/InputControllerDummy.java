0d23194d97b9dbff662ea093c02464b9df8eaeea
Bilal A <bila0765@stud.kea.dk>
2021-03-16 15:18:58 +0100

    "Mouseevent"

--- src/sample/Controller/InputControllerDummy.java
+++ src/sample/Controller/InputControllerDummy.java
@@ -1,11 +1,13 @@
 package sample.Controller;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class InputControllerDummy extends Application {
