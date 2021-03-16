d1bfb09abf2ad6ce262326ef779c4bb6926d9057
Bilal A <bila0765@stud.kea.dk>
2021-03-16 11:24:37 +0100

    "github test"

--- src/sample/RunGladiator.java
+++ src/sample/RunGladiator.java
@@ -1,19 +1,37 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class RunGladiator extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        File img = new File("sample/resources/hexagon.gif");
+        Image image = new Image(String.valueOf(img));
+
+        ImageView imageView = new ImageView(image);
+        imageView.setX(50);
+        imageView.setY(25);
+
+        imageView.setFitHeight(455);
+        imageView.setFitWidth(500);
+
+        imageView.setPreserveRatio(true);
+
+        Group root = new Group(imageView);
+
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+
     }
 //test
 
