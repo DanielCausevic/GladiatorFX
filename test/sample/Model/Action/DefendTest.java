d8370d7cb81d2db5b30d70279446053ee888736d
Thomas Simonsen <thom622g@stud.kea.dk>
2021-03-17 14:58:19 +0100

    tilføjet en test til at se om man tager hp hvis ens forsvar wep dør

--- test/sample/Model/Action/DefendTest.java
+++ test/sample/Model/Action/DefendTest.java
@@ -15,11 +15,13 @@ class DefendTest {
     Gladiator opponent;
     Weapon smallShield;
     Weapon shortSword1H;
+    Weapon testWeapon;
 
     @BeforeEach
     public void init(){
         smallShield = new Weapon("Small shield", 60, 6.0);
         shortSword1H = new Weapon("Short sword - 1H", 40, 8.0);
+        testWeapon = new Weapon("testWeapon", 2, 6.0);
 
         player = new Gladiator(null, 50, 50, new Point(330, 325));
         player.addShield(smallShield);
@@ -44,5 +46,14 @@ class DefendTest {
 
         //vil faile grundet random hitpoints.
         assertEquals(34, player.getMainWeapon().getHitPoints());
+    }
+
+    @Test
+    void defendTakingDamage() {
+        player.addWeaponToMain(testWeapon);
+        defend.defend(testWeapon, player, opponent);
+
+        //vil faile grundet random hitpoints.
+        assertEquals(96, player.getHP());
     }
 }
