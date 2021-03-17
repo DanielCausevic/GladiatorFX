46f5b22d127c0927ccb1fa3b6cbdafc4182c729e
Rasmus Skovbo <rasmusskovbo.dk@gmail.com>
2021-03-17 09:27:49 +0100

    Extended Sprite class in gladiator, added private denoms to variables, implm. getters & setters

--- src/sample/Model/Gladiator/Gladiator.java
+++ src/sample/Model/Gladiator/Gladiator.java
@@ -1,21 +1,45 @@
 package sample.Model.Gladiator;
 
+import javafx.scene.image.Image;
+import sample.Model.Sources.Sprite;
+
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-public class Gladiator {
-    short HP = 100;
-    short conditioning = 100;
-    ArrayList<Weapon> weapons = new ArrayList<>();
+public class Gladiator extends Sprite {
+    private short HP;
+    private short conditioning;
+    private ArrayList<Weapon> weapons = new ArrayList<>();
 
-    public Gladiator() {
-        this.weapons = new ArrayList<>();
+    public Gladiator(Image image, double width, double height,  ArrayList<Weapon> weapons) {
+        super(image, width, height);
         this.HP = 100;
         this.conditioning = 100;
-
+        this.weapons = weapons;
     }
 
-    public void addWeapon(Weapon weapon){
-        weapons.add(weapon);
+    // Getters & Setters
+    public short getHP() {
+        return HP;
+    }
+
+    public void setHP(short HP) {
+        this.HP = HP;
+    }
+
+    public short getConditioning() {
+        return conditioning;
+    }
+
+    public void setConditioning(short conditioning) {
+        this.conditioning = conditioning;
+    }
+
+    public ArrayList<Weapon> getWeapons() {
+        return weapons;
+    }
+
+    public void setWeapons(ArrayList<Weapon> weapons) {
+        this.weapons = weapons;
     }
 }
